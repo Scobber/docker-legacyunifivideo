79e66068cfd97a5136e4b42e2b43bdc1b397ae0b
Scobber <scott@pep.id.au>
2019-07-24 00:47:35 +1000

    Update startcontroller.sh

--- startcontroller.sh
+++ startcontroller.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Unifi Controller Services Stub"
-/usr/bin/jsvc -home /usr/lib/jvm/java-8-openjdk-amd64/jre -cp /usr/share/java/commons-daemon.jar:/usr/lib/unifi-video/lib/airvision.jar -procname unifi-video  -Djava.awt.headless=true -Dfile.encoding=UTF-8 -Xmx1024M -debug com.ubnt.airvision.Main start
+#/usr/bin/jsvc -home /usr/lib/jvm/java-8-openjdk-amd64/jre -cp /usr/share/java/commons-daemon.jar:/usr/lib/unifi-video/lib/airvision.jar -procname unifi-video  -Djava.awt.headless=true -Dfile.encoding=UTF-8 -Xmx1024M -debug com.ubnt.airvision.Main start
 sleep 15
 echo "Unifi Controller Web Stub"
+cd /usr/lib/unifi-video
 java -jar /usr/lib/unifi-video/lib/airvision.jar start
 sleep 15
 echo "Unifi Controller Server Logs"
