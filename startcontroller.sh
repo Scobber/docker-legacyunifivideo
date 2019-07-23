ce900b33ae8dc2967a748ff963101a401026fdbc
Scobber <scott@pep.id.au>
2019-07-24 00:15:19 +1000

    Update startcontroller.sh

--- startcontroller.sh
+++ startcontroller.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "Unifi Controller Stub"
 java -jar /usr/lib/unifi-video/lib/airvision.jar start
-tail -f /usr/lib/unifi-video/log/server.log
+sleep 15
+echo "Unifi Controller Server Logs"
+tail -f /usr/lib/unifi-video/logs/server.log
